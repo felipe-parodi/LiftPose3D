84c294abbd00a91af1aa8206e19444fbf588a436
user <semih.gunel@epfl.ch>
2020-08-14 16:50:33 +0200

    adding making figures

--- openmonkey/utils.py
+++ openmonkey/utils.py
@@ -22,7 +22,7 @@ def get_img_overlay(data_folder, i, name, label, joint_pairs, colors, points2d=N
     w = label[3]
     for j in range(13):
         _, frame_id, cam_id = parse_img_name(n)
-        #print(n, frame_id, cam_id)
+
         if points2d is None:
             jt_loc[j] = (label[5+2*j],label[4+2*j])
         else:
@@ -31,11 +31,40 @@ def get_img_overlay(data_folder, i, name, label, joint_pairs, colors, points2d=N
     for j, (jt1, jt2) in enumerate(joint_pairs):
         pt1 = jt_loc[jt1]
         pt2 = jt_loc[jt2]
+        cv2.line(image, pt1, pt2, colors[j], 3)
+    return image, n
+
+
+
+def get_img_overlay_matplotlib(ax, data_folder, i, name, label, joint_pairs, colors, points2d=None, cam_list=None, image=None):
+    label = label[i]
+    n = name[i][0][0]
+    if image is None:
+        img_name = data_folder + 'Images/{}'.format(n)
+        image = cv2.imread(img_name)
+    jt_loc = {}
+    h = label[2]
+    w = label[3]
+    for j in range(13):
+        _, frame_id, cam_id = parse_img_name(n)
+        #print(n, frame_id, cam_id)
+        if points2d is None:
+            jt_loc[j] = (label[5+2*j],label[4+2*j])
+        else:
+            jt_loc[j] = (int(points2d[cam_list.index(str(cam_id)), frame_id, j,0]), int(points2d[cam_list.index(str(cam_id)), frame_id, j, 1]))
+    
+    
+    ax.imshow(image)
+    for j, (jt1, jt2) in enumerate(joint_pairs):
+        pt1 = jt_loc[jt1]
+        pt2 = jt_loc[jt2]
         #if pt1[0] < 1 or pt1[1] < 1 or pt2[0] < 1 or pt2[1] < 1 or pt1[0] > w or pt1[1] > h or pt2[0] > w or pt2[1] > h:
         #    continue
         #else:
-        cv2.line(image, pt1, pt2, colors[j], 3)
+        #cv2.line(image, pt1, pt2, colors[j], 3)
+        ax.plot([pt1[0], pt2[0]], [pt1[1], pt2[1]], c=[c_/255 for c_ in colors[j]])
     return image, n
+
 
 
 # batch(batch#)_(frame#)_(cameraID).jpg , Eg. batch7_000003120_18064162.jpg
